c10ea86bdc0a1d8c712bf8617efde709ebdcd1e0
Subash7Lingden <subashsubbalingden@gmail.com>
2024-05-06 15:02:34 +0545

    data ingestion completed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Subash7Lingden",
+    author_email="subashsubbalingden@gmail.com",
     packages=find_packages()
 )
--- us_visa/configuration/mongo_db_connection.py
+++ us_visa/configuration/mongo_db_connection.py
@@ -1,3 +1,4 @@
+
 import sys
 
 from us_visa.exception import USvisaException
